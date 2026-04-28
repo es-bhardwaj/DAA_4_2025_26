0cb624ee72f8f40ffbd546b6b6caaa8579fc2d7b
Esshaan Bhardwaj <ebksahal@gmail.com>
2026-04-28 14:18:30 +0530

    initial commit
    Refactored LCS function to return length and modified main to accept user input.

--- Lab-extrernal/ques2.cpp
+++ Lab-extrernal/ques2.cpp
@@ -2,44 +2,23 @@
 #include <vector>
 using namespace std;
 
-string lcs(string s1, string s2) {
-    int m = s1.length();
-    int n = s2.length();
-    vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
-
-    for (int i = 1; i <= m; i++) {
-        for (int j = 1; j <= n; j++) {
-            if (s1[i - 1] == s2[j - 1])
-                dp[i][j] = 1 + dp[i - 1][j - 1];
+int lcs(string s1,string s2){
+    int n=s1.length(),m=s2.length();
+    vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+            if(s1[i-1]==s2[j-1])
+                dp[i][j]=1+dp[i-1][j-1];
             else
-                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
         }
     }
-
-    int i = m, j = n;
-    string result = "";
-
-    while (i > 0 && j > 0) {
-        if (s1[i - 1] == s2[j - 1]) {
-            result = s1[i - 1] + result;
-            i--; j--;
-        } else if (dp[i - 1][j] > dp[i][j - 1]) {
-            i--;
-        } else {
-            j--;
-        }
-    }
-    return result;
+    return dp[n][m];
 }
 
-int main() {
-    string s1 = "abcde";
-    string s2 = "ace";
-
-    string result = lcs(s1, s2);
-
-    cout << "Longest Common Subsequence: " << result << endl;
-    cout << "Length: " << result.length() << endl;
-
+int main(){
+    string s1,s2;
+    cin>>s1>>s2;
+    cout<<lcs(s1,s2);
     return 0;
 }
